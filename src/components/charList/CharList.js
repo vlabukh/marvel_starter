929131181e7627f9771d449bc9566d3e02284163
Vlad Bukh <vlabukh@gmail.com>
2022-01-23 03:32:26 +0500

    119 prop-types - проверка соответствия типам данных

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner'
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import MarvelService from '../../services/MarvelService'
@@ -106,6 +107,10 @@ class CharList extends Component {
       </div>
     )
   }
+}
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList
